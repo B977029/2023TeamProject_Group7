0565e0ed8ad23e65882d80f075973c4d1825fce5
JuneSu001 <48229660+JuneSu001@users.noreply.github.com>
2023-05-14 19:57:36 +0900

    start option exit
    asdf

---
+++ UnityProjectFile/Assets/Script/MainMenu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ClickExit()
+    {
+        Application.Quit();
+    }
+    public void ClickStart()
+    {
+        SceneManager.LoadScene("Start");
+    }
+    
+    public void ClickOption()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
