5c4a38668c9571a06162711a4c56637bd0d949bd
JuneSu001 <white163171@gmail.com>
2023-06-21 15:59:38 +0900

    카메라 범위 제한+플레이어 맵 밖으로 못나가게하기

--- UnityProjectFile/Assets/Script/CameraController.cs
+++ UnityProjectFile/Assets/Script/CameraController.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
+    private Vector3 pos;
+    float minxPosition=0;
+    float maxxPosition=66;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    float cameraMoveSpeed;
+    
+    Vector3 cameraPosition = new Vector3(0, 0, -10);
+    void FixedUpdate()
     {
-        transform.position = player.transform.position+ new Vector3(0, 0, -10);
+        LimitCamera();
+    }
+    void LimitCamera()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.transform.position + cameraPosition, Time.deltaTime * cameraMoveSpeed);//플레이어 따라 이동
+
+
+        pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, minxPosition, maxxPosition);
+        pos.y = Mathf.Clamp(pos.y, minxPosition, minxPosition);
+        transform.position = pos;
     }
 }
