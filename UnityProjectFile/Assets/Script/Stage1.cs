45f8fcba845efc4e16f0bb59d89ed0a868442b9c
JuneSu001 <white163171@gmail.com>
2023-06-23 15:59:43 +0900

    stage1 UI, 이벤트 추가
    고사리 채집과 채집 후 이벤트 추가

--- UnityProjectFile/Assets/Script/Stage1.cs
+++ UnityProjectFile/Assets/Script/Stage1.cs
@@ -14,7 +14,40 @@ public class Stage1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (GameObject.Find("UI").transform.Find("text").gameObject.activeSelf == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                GameObject.Find("UI").transform.Find("gosariend").gameObject.SetActive(true);
+                GameObject.Find("UI").transform.Find("gosari").gameObject.SetActive(false);
+                GameObject.Find("CraftHint").transform.Find("one").gameObject.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Move1-2")
+        {
+            
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == GameObject.Find("UI").transform.Find("gosari").name)
+        {
+            GameObject.Find("UI").transform.Find("text").gameObject.SetActive(true);
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == GameObject.Find("UI").transform.Find("gosari").name)
+
+        {
+            GameObject.Find("UI").transform.Find("text").gameObject.SetActive(false);
+        }
     }
 
     //Debug
